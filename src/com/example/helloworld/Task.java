03a5053db74f167a26a9d2e610be0cce81614255
niraliK08 <125687080+niraliK08@users.noreply.github.com>
2024-07-06 01:05:45 -0500

    Update Task.java

--- src/com/example/helloworld/Task.java
+++ src/com/example/helloworld/Task.java
@@ -1,25 +1,25 @@
 package com.example.helloworld;
 
 public class Task extends HelloWorld {
-    private String name;
-    private String description;
-    private int priority;
+        private String name;
+        private String description;
+        private int priority;
 
-    public Task(String name, String description, int priority) {
-        this.name = name;
-        this.description = description;
-        this.priority = priority;
-    }
+        public Task(String name, String description, int priority) {
+            this.name = name;
+            this.description = description;
+            this.priority = priority;
+        }
 
-    public String getName() {
-        return name;
-    }
+        public String getName() {
+            return name;
+        }
 
-    public String getDescription () {
-        return description;
-    }
+        public String getDescription() {
+            return description;
+        }
 
-    public int getPriority() {
-        return priority;
+        public int getPriority() {
+            return priority;
+        }
     }
-}
